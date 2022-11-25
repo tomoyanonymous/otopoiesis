828c2c57439a46180542b427b4cd265d52a81e65
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-11-25 16:10:53 +0900

    graph layout

--- src/gui/region.rs
+++ src/gui/region.rs
@@ -28,20 +28,6 @@ impl egui::Widget for &UiBar {
     }
 }
 
-fn default_graph(
-    label: impl std::hash::Hash,
-    iter: impl Iterator<Item = egui::plot::Value>,
-) -> egui::plot::Plot {
-    let line = egui::plot::Line::new(egui::plot::Values::from_values_iter(iter));
-    egui::plot::Plot::new(label)
-        .line(line)
-        .allow_drag(false)
-        .allow_zoom(false)
-        .show_x(false)
-        .show_y(false)
-        .min_size(egui::vec2(0.,0.))
-}
-
 impl egui::Widget for &mut Model {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
         let scaling_factor = super::SAMPLES_PER_PIXEL_DEFAULT;
@@ -70,24 +56,28 @@ impl egui::Widget for &mut Model {
                             .max(end.max(maxsize) - maxsize);
                         self.params.range.set_start(new_start);
                     }
-
-                    //draw main plot
-                    let graph_origin = default_graph(
-                        "region",
-                        self.samples.iter().enumerate().map(|(i, s)| {
-                            let x = nannou::math::map_range(
-                                i as f32,
-                                0.,
-                                self.samples.len() as f32,
-                                0.,
-                                x_size,
-                            );
-                            let y = *s * y_size * self.get_current_amp();
-                            egui::plot::Value::new(x, y)
-                        }),
-                    );
+                    let iter = self.samples.iter().enumerate().map(|(i, s)| {
+                        let x = nannou::math::map_range(
+                            i as f32,
+                            0.,
+                            self.samples.len() as f32,
+                            0.,
+                            x_size,
+                        );
+                        let y = *s * y_size * self.get_current_amp();
+                        egui::plot::Value::new(x, y)
+                    });
+                    let line = egui::plot::Line::new(egui::plot::Values::from_values_iter(iter));
+                    let graph_component = egui::plot::Plot::new("region")
+                        .line(line)
+                        .allow_drag(false)
+                        .allow_zoom(false)
+                        .show_x(false)
+                        .show_y(false)
+                        .show_axes([false, true])
+                        .min_size(egui::vec2(0., 0.));
                     let graph = ui
-                        .add_sized(region_size, graph_origin)
+                        .add_sized(region_size, graph_component)
                         .interact(egui::Sense::hover());
 
                     // ui.painter_at(lrect)
