4e2b9bae92a499b9493f20bbd7a2ae15ece69bc2
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-12-27 14:08:11 +0900

    fixed some definiton in action

--- src/gui.rs
+++ src/gui.rs
@@ -1,13 +1,13 @@
-//! GUI Definitions for parameter control and visualization for application. 
+//! GUI Definitions for parameter control and visualization for application.
 //! Currently implemented on [`egui`].
 
-pub mod timeline;
-pub mod region;
-pub mod track;
-pub mod generator;
 pub mod app;
+pub mod generator;
+pub mod region;
+pub mod timeline;
+pub mod track;
 pub mod transport;
 
-pub(crate) const SAMPLES_PER_PIXEL_DEFAULT:f32= 100.0;
+pub(crate) const SAMPLES_PER_PIXEL_DEFAULT: f32 = 100.0;
 
-pub(crate) const TRACK_HEIGHT:f32= 100.0;
+pub(crate) const TRACK_HEIGHT: f32 = 100.0;
