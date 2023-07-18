d760bcd53c4f6012df4e0f5e0883a720ff9010c4
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2023-07-18 23:29:00 +0900

    fixed fade handle interaction

--- src/gui/region/regionfilter/fadeinout.rs
+++ src/gui/region/regionfilter/fadeinout.rs
@@ -57,12 +57,13 @@ impl<'a> egui::Widget for FadeHandle<'a> {
 
         let mut make_circle = |w, is_start: bool| {
             let radius = 5.0;
+            let top = target_rect.top();
             let (rect, handle_pos) = if is_start {
-                let handle_pos = egui::pos2(target_rect.left() + w, target_rect.top());
+                let handle_pos = egui::pos2(target_rect.left() + w, top + radius * 2.0);
                 let rect = egui::Rect::from_points(&[target_rect.left_bottom(), handle_pos]);
                 (rect, handle_pos)
             } else {
-                let handle_pos = egui::pos2(target_rect.right() - w, target_rect.top());
+                let handle_pos = egui::pos2(target_rect.right() - w, top + radius * 2.0);
                 let rect = egui::Rect::from_points(&[handle_pos, target_rect.right_bottom()]);
                 (rect, handle_pos)
             };
@@ -78,9 +79,9 @@ impl<'a> egui::Widget for FadeHandle<'a> {
 
             let c = egui::Color32::DARK_GRAY;
             let points = if is_start {
-                [rect.left_bottom(), rect.right_top()]
+                [rect.left_bottom(), handle_pos]
             } else {
-                [rect.left_top(), rect.right_bottom()]
+                [handle_pos, rect.right_bottom()]
             };
 
             painter.line_segment(points, egui::Stroke::new(1.0, c));
@@ -88,20 +89,30 @@ impl<'a> egui::Widget for FadeHandle<'a> {
 
             ui_handle.on_hover_cursor(egui::CursorIcon::PointingHand)
         };
-        let range_pix = (self.state.range.end() - self.state.range.start()) as f32
-            * gui::PIXELS_PER_SEC_DEFAULT;
-        let scale = move |sec| sec * gui::PIXELS_PER_SEC_DEFAULT;
-        let descale = move |pix| gui::PIXELS_PER_SEC_DEFAULT / pix;
-        let in_width = scale(self.param.time_in.load());
-        let out_width = scale(self.param.time_out.load());
+        let range_sec = self.state.range.end() - self.state.range.start();
+        let range_pix = range_sec as f32 * gui::PIXELS_PER_SEC_DEFAULT;
+        let sec_to_pix = move |sec| sec * gui::PIXELS_PER_SEC_DEFAULT;
+        let pix_to_sec = move |pix| pix / gui::PIXELS_PER_SEC_DEFAULT;
+        let time_in = self.param.time_in.load();
+        let time_out = self.param.time_out.load();
+
+        // to prevent crossing fade handles when the region length was modified.
+        if time_in + time_out > range_sec as f32 {
+            let delta = time_in + time_out - range_sec as f32;
+            self.param.time_in.store(time_in - delta / 2.0);
+            self.param.time_out.store(time_out - delta / 2.0);
+        }
+        let in_width = sec_to_pix(self.param.time_in.load());
+        let out_width = sec_to_pix(self.param.time_out.load());
         let start = make_circle(in_width, true);
         if start.drag_started() {
             self.state.start_tmp = in_width;
         }
         if start.dragged() {
             self.state.start_tmp += start.drag_delta().x;
-            let v = (self.state.start_tmp + start.drag_delta().x).clamp(0.0, range_pix - out_width);
-            self.param.time_in.store(descale(v));
+            let bound = range_pix - out_width;
+            let v = (self.state.start_tmp + start.drag_delta().x).clamp(0.0, bound);
+            self.param.time_in.store(pix_to_sec(v));
         }
         if start.drag_released() {
             self.state.start_tmp = 0.0;
@@ -114,12 +125,23 @@ impl<'a> egui::Widget for FadeHandle<'a> {
             //accumlate delta.
             self.state.end_tmp -= end.drag_delta().x;
             let v = (self.state.end_tmp - end.drag_delta().x).clamp(0.0, range_pix - in_width);
-
-            self.param.time_out.store(descale(v));
+            self.param.time_out.store(pix_to_sec(v));
         }
         if end.drag_released() {
             self.state.end_tmp = 0.0;
         }
         start.union(end)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn scaling() {
+        let sec_to_pix = move |sec| sec * gui::PIXELS_PER_SEC_DEFAULT;
+        let pix_to_sec = move |pix| pix / gui::PIXELS_PER_SEC_DEFAULT;
+        let input_sec: f32 = 30.0;
+        assert_eq!(input_sec, pix_to_sec(sec_to_pix(input_sec)));
     }
 }
